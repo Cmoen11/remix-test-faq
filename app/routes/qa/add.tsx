42f5299df450e56c4a17b1e38e79ffe302b608ba
Christian Moen <cmoen11@gmail.com>
2023-02-17 06:52:11 +0000

    added server side validation

--- app/routes/qa/add.tsx
+++ app/routes/qa/add.tsx
@@ -1,8 +1,13 @@
-import type { ActionArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
-import { Form, Link, useTransition } from "@remix-run/react";
+import { ActionArgs, json, redirect } from "@remix-run/node";
+import { Form, Link, useActionData, useTransition } from "@remix-run/react";
 import { useRef } from "react";
+import * as yup from "yup";
 import { insertFaq } from "~/services/FaqService";
+
+const schema = yup.object().shape({
+    question: yup.string().required().min(5),
+    answer: yup.string().required().min(10),
+});
 
 export async function action({ request }: ActionArgs) {
     const body = await request.formData();
@@ -10,14 +15,25 @@ export async function action({ request }: ActionArgs) {
     const question = body.get("question");
     const answer = body.get("answer");
 
-    console.log("action is called")
 
-    if (question && answer) {
-        await insertFaq({
-            question: question.toString(),
-            answer: answer.toString(),
-        });
+    const isValid = await schema.isValid({
+        question,
+        answer,
+    });
+
+    if (!isValid) {
+        return json({
+            error: {
+                answer: "Svaret må være minst 10 tegn",
+                question: "Spørsmålet må være minst 5 tegn",
+            }
+        })
     }
+
+    await insertFaq({
+        question: question!.toString(),
+        answer: answer!.toString(),
+    });
 
     return redirect("/qa");
 }
@@ -26,24 +42,30 @@ export async function action({ request }: ActionArgs) {
 function Add() {
     const transition = useTransition();
     const formRef = useRef<HTMLFormElement>(null);
+    const actionResult = useActionData<typeof action>();
 
 
     let isAdding = transition.state === "submitting";
 
     return (
         <div className="flex-col w-full mx-auto max-md:w-full max-md:px-5">
-            {transition.state === "submitting" && (
-                <p className="text-yellow-500">
-                    Legger til spørsmålet...
-                </p>
-            )}
-            {transition.state === "loading" && (
-                <p className="text-green-500">Spørsmålet er lagt til!</p>
-            )}
+
 
             <Form method="post" className="flex-col mx-auto max-md:w-full max-md:py-5" ref={formRef}>
-                <input type="text" disabled={isAdding} autoFocus className="bg-gray-100 p-2 rounded-md shadow-md mt-5 mb-0 ease-in-out relative hover:bg-gray-200 w-full" name="question" placeholder="Skriv inn spørsmål" />
-                <textarea disabled={isAdding} className="bg-gray-100 p-2 rounded-md shadow-md mt-5 mb-0 ease-in-out relative hover:bg-gray-200 w-full" name="answer" placeholder="Skriv inn svar" />
+                <div>
+
+                    <input type="text" disabled={isAdding} autoFocus className="bg-gray-100 p-2 rounded-md shadow-md mt-5 mb-0 ease-in-out relative hover:bg-gray-200 w-full" name="question" placeholder="Skriv inn spørsmål" />
+                    {actionResult?.error?.question && (
+                        <p className="text-red-500">{actionResult.error.question}</p>
+                    )}
+                </div>
+                <div>
+                    <textarea disabled={isAdding} className="bg-gray-100 p-2 rounded-md shadow-md mt-5 mb-0 ease-in-out relative hover:bg-gray-200 w-full" name="answer" placeholder="Skriv inn svar" />
+
+                    {actionResult?.error?.answer && (
+                        <p className="text-red-500">{actionResult.error.answer}</p>
+                    )}
+                </div>
 
                 <section className="flex flex-row gap-5">
                     <button disabled={isAdding} className="bg-green-100 p-2 rounded-md shadow-md mt-5 mb-0 ease-in-out relative hover:bg-gray-200 w-auto" type="submit">
@@ -53,10 +75,30 @@ function Add() {
                         Avbryt
                     </Link>
                 </section>
+                {transition.state === "submitting" && (
+                    <p className="text-yellow-500">
+                        Legger til spørsmålet...
+                    </p>
+                )}
+                {transition.state === "loading" && (
+                    <p className="text-green-500">Spørsmålet er lagt til!</p>
+                )}
             </Form>
 
         </div>
+    );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+
+    return (
+        <div className="flex-col w-full mx-auto max-md:w-full max-md:px-5" >
+            <p className="text-red-500">
+                {error.message}
+            </p>
+        </div>
     )
-};
+}
+
 
 export default Add;
